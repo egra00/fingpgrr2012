77a850fd80a07a830999125e61d7f76467c9f732
saenz.emi.jos@gmail.com <saenz.emi.jos@gmail.com@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2012-05-11 19:19:15 +0000

    Agrego CBGPDump

---
+++ xTotem/src/be/ac/ulg/montefiore/run/totem/repository/rrloc/tools/CBGPDump/CBGPDumpAlgorithm.java
@@ -0,0 +1,46 @@
+package be.ac.ulg.montefiore.run.totem.repository.rrloc.tools.CBGPDump;
+
+import java.util.HashMap;
+
+import be.ac.ulg.montefiore.run.totem.repository.rrloc.iAlgorithm.RRLocAlgorithm;
+
+public class CBGPDumpAlgorithm implements RRLocAlgorithm
+{
+
+	@Override
+	public void dump() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void log() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setParameters(HashMap params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
