da0c8bb4387e47e19a29d717191f81039ad2e6fc
saenz.emi.jos@gmail.com <saenz.emi.jos@gmail.com@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2012-05-11 18:36:27 +0000

    ya lo cargo otra vez

--- xTotem/src/be/ac/ulg/montefiore/run/totem/repository/rrloc/algorithms/bgpsep/iBGPSessionType.java
+++
@@ -1,6 +0,0 @@
-package be.ac.ulg.montefiore.run.totem.repository.rrloc.algorithms.bgpsep;
-
-public enum iBGPSessionType {
-	peer,
-	client,
-}
--- xTotem/src/be/ac/ulg/montefiore/run/totem/repository/rrloc/iAlgorithm/RRLocAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package be.ac.ulg.montefiore.run.totem.repository.rrloc.iAlgorithm;
-
-public interface RRLocAlgorithm {
-	
-	public void init();
-	public void run(Object in_params, Object out_resutl);
-	public void stop();
-
-}
