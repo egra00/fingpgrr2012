4d603f85f44174908da5f96903639cbe2166fc41
saenz.emi.jos@gmail.com <saenz.emi.jos@gmail.com@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2012-05-13 16:55:40 +0000

    jjjjj

--- xTotem/src/be/ac/ulg/montefiore/run/totem/repository/rrloc/tools/CBGPDump/CBGPDump.java
+++ xTotem/src/be/ac/ulg/montefiore/run/totem/repository/rrloc/tools/CBGPDump/CBGPDump.java
@@ -1,11 +1,24 @@
 package be.ac.ulg.montefiore.run.totem.repository.rrloc.tools.CBGPDump;
 
+import be.ac.ulg.montefiore.run.totem.repository.model.exception.AlgorithmParameterException;
+import be.ac.ulg.montefiore.run.totem.repository.rrloc.tools.CBGPDump.iface.BindCBGPDumpAlgorithm;
+import be.ac.ulg.montefiore.run.totem.util.ParameterDescriptor;
 
-public class CBGPDump
+public class CBGPDump extends BindCBGPDumpAlgorithm 
 {
     public CBGPDump()
     {
-
+    	algorithm =  new CBGPDumpAlgorithm();
+    	algorithm.init();
+    	try 
+    	{
+			params.add(new ParameterDescriptor("ASID", "Domain ASID (leave blank for default)", Integer.class, null));
+			params.add(new ParameterDescriptor("FILENAME", "Name out file", String.class, "scriptCBGP.cli"));
+		} 
+    	catch (AlgorithmParameterException e) 
+		{
+			e.printStackTrace();
+		}
     	
     }
 }
