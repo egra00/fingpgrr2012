63d75cdaa210ded6db68e04b0f93a5a0c2b34ed5
saenz.emi.jos@gmail.com <saenz.emi.jos@gmail.com@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2012-06-11 00:20:44 +0000

    Implemente el grafo separador original (usado en BGPSep) funciona mal (al punto tal que NO genera grafos separadpores), el algoritmo no fue mal interpretado si no que utiliza demaciadas aproximaciones, lo que posiblemnete haga que funcione mal

--- xTotem/src/uy/edu/fing/repository/rrloc/graphTools/Separator.java
+++ xTotem/src/uy/edu/fing/repository/rrloc/graphTools/Separator.java
@@ -1,8 +1,16 @@
 package uy.edu.fing.repository.rrloc.graphTools;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+
+import org.ejml.alg.dense.decomposition.SingularMatrixException;
+import org.ejml.simple.SimpleMatrix;
 
 import agape.algos.Separators;
 import agape.tools.Components;
@@ -38,6 +46,240 @@ public class Separator {
 		}
 		
 		return ret;
+	}
+	
+	static public GraphSeparator SpectralBisection(Graph<Node,Link> G) 
+	{ 
+		GraphSeparator SG = new GraphSeparator();
+		Graph<Node, Link> V1 = new UndirectedSparseGraph<Node, Link>();
+		Graph<Node, Link> V2 = new UndirectedSparseGraph<Node, Link>();
+		HashMap<Node, Double> hmap = new HashMap<Node, Double>();
+		
+		double fv[] = Fiedler_Vector(G, 100);
+		
+		double median = 0;
+		for(int i = 0; i<fv.length ; i++)
+			median += fv[i];
+		median = median/fv.length;
+		
+		int i=0;
+		for (Node v : G.getVertices()) 
+		{
+			if (fv[i]<= median)
+			{
+				V1.addVertex(v);
+				hmap.put(v, fv[i]);
+			}
+			else
+			{
+				V2.addVertex(v);
+				hmap.put(v, fv[i]);
+			}
+			i++;
+		}
+		
+		if (V1.getVertexCount() - V2.getVertexCount() > 1)
+		{
+			int dif = V1.getVertexCount() - V2.getVertexCount();
+			Collection<Node> col_nodes = V1.getVertices();
+			List<Node> l_aux = new LinkedList<Node>();
+			
+			for (Node v : col_nodes) 
+			{
+				i=0;
+				Iterator<Node> ii = l_aux.iterator();
+				while( ii.hasNext() && (Math.abs(median - hmap.get(v)) >= Math.abs(median - hmap.get(ii.next()))))
+					i++;
+				l_aux.add(i, v);
+			}
+			
+			Iterator<Node> ii = l_aux.iterator();
+			for (int j = 0; j<(dif/2); j++)
+			{
+				Node n = ii.next();
+				V2.addVertex(n);
+				V1.removeVertex(n);
+			}
+				
+		}
+		else if (V2.getVertexCount() - V1.getVertexCount() > 1)
+		{
+			int dif = V2.getVertexCount() - V1.getVertexCount();
+			Collection<Node> col_nodes = V2.getVertices();
+			List<Node> l_aux = new LinkedList<Node>();
+			
+			for (Node v : col_nodes) 
+			{
+				i=0;
+				Iterator<Node> ii = l_aux.iterator();
+				while( ii.hasNext() && (Math.abs(median - hmap.get(v)) >= Math.abs(median - hmap.get(ii.next()))))
+					i++;
+				l_aux.add(i, v);
+			}
+			
+			Iterator<Node> ii = l_aux.iterator();
+			for (int j = 0; j<(dif/2); j++)
+			{
+				Node n = ii.next();
+				V1.addVertex(n);
+				V2.removeVertex(n);
+			}
+		}
+			
+		Graph<Node, Link> V1p = new UndirectedSparseGraph<Node, Link>();
+		Graph<Node, Link> V2p = new UndirectedSparseGraph<Node, Link>();
+		for(Node n : V1.getVertices())
+		{
+			Collection<Node> col_ne = G.getNeighbors(n);
+			for(Node v : col_ne)
+			{
+				if (V2.containsVertex(v)) 
+				{	
+					if (!V2p.containsVertex(v)) V2p.addVertex(v);
+					V1p.addVertex(n);
+				}
+			}
+			
+		}
+		
+		Graph<Node, Link> aux1;
+		Graph<Node, Link> aux2;
+		
+		if (V1p.getVertexCount() <= V2p.getVertexCount())
+		{
+			aux1 = V1p;
+			aux2 = V1;
+		}
+		else
+		{
+			aux1 = V2p;
+			aux2 = V2;
+		}
+		
+		for(Node n : aux1.getVertices())
+		{
+			aux2.removeVertex(n);
+		}
+		
+		// Set separator
+		Set<Node> set =  new HashSet<Node>();
+		for(Node n : V1p.getVertices())
+		{
+			set.add(n);
+			System.out.println(n.getDescription());
+		}
+		SG.setSeparator(set);
+		
+		
+		// Components
+		List<Graph<Node, Link>> components = new LinkedList<Graph<Node, Link>>();
+		components.add(V1);
+		components.add(V2);
+		SG.setComponents(components);
+		
+		
+		return SG;
+	}
+	
+	static private double[] Fiedler_Vector(Graph<Node,Link> G, int k)
+	{
+		int max_degree = Integer.MIN_VALUE;
+		double L[][] = new double[G.getVertexCount()][G.getVertexCount()];
+		double fv[] = new double[G.getVertexCount()];
+		int i = 0;
+		int j = 0;
+		for(Node u : G.getVertices())
+		{
+			j = 0;
+			for(Node v : G.getVertices())
+			{
+				if(i==j)
+				{
+					L[i][j] = G.outDegree(u); 
+					if(G.outDegree(u) >= max_degree) max_degree = G.outDegree(u);
+				}
+				else if (i!=j && G.isNeighbor(u, v))
+				{
+					L[i][j] = -1;
+				}
+				else
+				{
+					L[i][j] = 0;
+				}
+				j++;
+			}
+			i++;
+		}
+		
+		double FiedlerNumber = 8*max_degree/(double)G.getVertexCount();
+		double x[] = new double[G.getVertexCount()];
+		
+		for(i = 0; i<G.getVertexCount(); i++)
+		{
+			L[i][i] -= FiedlerNumber;
+		}
+		
+		
+		
+		for(i=0; i < k; i++)
+		{
+			//Norma
+			double norma = 0;
+			for(int h=0; h<G.getVertexCount(); h++)
+			{
+				x[h] = Math.random();
+				norma += x[h]*x[h];
+			}
+			norma=Math.sqrt(norma);
+			
+			
+			for(int g = 0; g<G.getVertexCount(); g++)
+				fv[g]= (1/norma)*x[g];
+			
+			x=Solve_Ecuation_System(L, fv);
+		}
+		
+		return fv;
+	}
+	
+	static private double[] Solve_Ecuation_System(double[][] A, double[] b )
+	{
+		SimpleMatrix AA = new SimpleMatrix(b.length, b.length);
+		SimpleMatrix bb = new SimpleMatrix(b.length, 1);
+		
+		for(int i = 0; i<b.length; i++)
+		{
+			for(int j = 0; j<b.length; j++)
+			{
+				AA.set(i, j, A[i][j]);
+			}
+		}
+		
+		for(int i = 0; i<b.length; i++)
+		{
+			bb.set(i, b[i]);
+		}
+		
+		try
+		{
+			SimpleMatrix xx = AA.solve(bb);
+			
+			double x[] = new double[b.length];
+			
+			for(int i = 0; i<b.length; i++)
+			{
+				x[i]=xx.get(i);
+			}
+			
+			return x;
+		}
+		catch (SingularMatrixException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+
 	}
 	
 }
