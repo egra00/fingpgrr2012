8fce24ad1023a8250ec87ea105294829d45b9b53
saenz.emi.jos <saenz.emi.jos@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2012-08-16 17:47:48 +0000

    Los hago andar basicamnete, por mas detalles consultarme personalmente. La correccion mas importanet es hecha en BGPSepS(...). Error muy rebuscado y dificil de encontrar (pero lo encontre!!...... me estoy para delante a mi mismo).

--- xTotem/src/uy/edu/fing/repository/rrloc/algorithms/bgpsepX/bgpsepD/BGPSepDAlgorithm.java
+++ xTotem/src/uy/edu/fing/repository/rrloc/algorithms/bgpsepX/bgpsepD/BGPSepDAlgorithm.java
@@ -3,8 +3,6 @@ package uy.edu.fing.repository.rrloc.algorithms.bgpsepX.bgpsepD;
 import java.util.Collection;
 import java.util.List;
 
-import org.apache.log4j.Logger;
-
 import uy.edu.fing.repository.rrloc.algorithms.iBGPSession;
 import uy.edu.fing.repository.rrloc.algorithms.iBGPSessionType;
 import uy.edu.fing.repository.rrloc.algorithms.bgpsepX.bgpsep.BGPSepAlgorithm;
@@ -16,47 +14,43 @@ import edu.uci.ics.jung2.graph.Graph;
 
 public class BGPSepDAlgorithm implements RRLocAlgorithm {
 	
-	private Logger my_logger;
-	
 	@SuppressWarnings("unchecked")
 	@Override
 	public void run(Object param, Object result) {
-		my_logger = Logger.getLogger(BGPSepDAlgorithm.class);
 		Graph<Node, Link> G = (Graph<Node, Link>) param;
 		List<iBGPSession> I = (List<iBGPSession>) result;
 		
-		Graph<Node, Link> Gp = Operations.copyUndirectedSparseGraph(G);
-		removePedantVertexes(G, I, Gp);
+		Graph<Node, Link> Gp = removePedantVertexes(G, I);
 		RRLocAlgorithm bgpsep = new BGPSepAlgorithm();
 		bgpsep.run(Gp, I);
 	}
 	
 	
-	protected void removePedantVertexes(Graph<Node, Link> G, List<iBGPSession> I, Graph<Node, Link> Gp) 
+	protected Graph<Node, Link> removePedantVertexes(Graph<Node, Link> _G, List<iBGPSession> _out_I) 
 	{
 		boolean pending = true;
+		Graph<Node, Link> Gaux;
+		Graph<Node, Link> Gp = Operations.copyUndirectedSparseGraph(_G);
 		
 		while(pending)
 		{
-			G = Operations.copyUndirectedSparseGraph(Gp);
+			Gaux = Operations.copyUndirectedSparseGraph(Gp);
 			pending = false;
 			
-			for (Node n : G.getVertices()) 
+			for (Node n : Gaux.getVertices()) 
 			{
-				if ((G.inDegree(n) == G.outDegree(n)) && G.outDegree(n) == 1)
+				if (Gaux.degree(n) == 1)
 				{
-					Collection<Node> out_neighbor = G.getNeighbors(n);
+					Collection<Node> out_neighbor = Gaux.getNeighbors(n);
 					Node neighbor = out_neighbor.iterator().next();
-					I.add(new iBGPSession(n.getId(), neighbor.getId(), iBGPSessionType.client));
+					_out_I.add(new iBGPSession(n.getId(), neighbor.getId(), iBGPSessionType.client));
 					Gp.removeVertex(n);
 					pending = true;
-				}
-				else if (G.inDegree(n) != G.outDegree(n))
-				{
-					my_logger.info("[WARNING]: In node "+ n.getId() + " in-degree != out-degree (used out-degree)");
 				}	
 			}
 		}
+		
+		return Gp;
 	}
 	
 	/*
--- xTotem/src/uy/edu/fing/repository/rrloc/algorithms/bgpsepX/bgpsepS/BGPSepSAlgorithm.java
+++ xTotem/src/uy/edu/fing/repository/rrloc/algorithms/bgpsepX/bgpsepS/BGPSepSAlgorithm.java
@@ -1,6 +1,8 @@
 package uy.edu.fing.repository.rrloc.algorithms.bgpsepX.bgpsepS;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -9,7 +11,6 @@ import uy.edu.fing.repository.rrloc.algorithms.iBGPSessionType;
 import uy.edu.fing.repository.rrloc.algorithms.bgpsepX.bgpsepD.BGPSepDAlgorithm;
 import uy.edu.fing.repository.rrloc.graphTools.GraphSeparator;
 import uy.edu.fing.repository.rrloc.graphTools.Separator;
-import agape.tools.Operations;
 import be.ac.ulg.montefiore.run.totem.domain.exception.NodeNotFoundException;
 import be.ac.ulg.montefiore.run.totem.domain.model.Link;
 import be.ac.ulg.montefiore.run.totem.domain.model.Node;
@@ -19,6 +20,60 @@ import edu.uci.ics.jung2.graph.Graph;
 @SuppressWarnings("unchecked")
 public class BGPSepSAlgorithm extends BGPSepDAlgorithm {
 
+	private DijkstraShortestPath<Node, Link> dijkstra;
+
+	
+	public boolean contiene(List<iBGPSession> lst, String n1, String n2)
+	{
+		for(iBGPSession session : lst)
+		{
+			if (session.getIdLink1().equals(n1) && session.getIdLink2().equals(n2) ||
+				session.getIdLink1().equals(n2) && session.getIdLink2().equals(n1))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public Node getNode(Link link, Node node) // Dado un link "link" y un nodo "node" doy el extremo del que no es "node"
+	{
+		try 
+		{
+			if (link.getDstNode() != node)
+				return link.getDstNode();
+			else
+				return link.getSrcNode();
+		}
+		catch (NodeNotFoundException e) 
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+	public List<Node> toPathNode(Node u, Node v)
+	{
+		List<Node> lst = new LinkedList<Node>();
+		List<Link> path = dijkstra.getPath(u, v);
+		
+		Node u_i = u;
+		lst.add(u_i);
+		
+		for(Link link : path)
+		{
+			Node u_i_mas_1 = getNode(link, u_i);
+			u_i = u_i_mas_1;
+			lst.add(u_i);
+		}
+		
+		return lst;
+	}
+	
+	
+	
 	@Override
 	public void run(Object param, Object result) {
 		Graph<Node, Link> G = (Graph<Node, Link>) param;
@@ -26,33 +81,32 @@ public class BGPSepSAlgorithm extends BGPSepDAlgorithm {
 		
 		/* Step 1: removing the pendant vertexes gradually*/
 		
-		Graph<Node, Link> Gp = Operations.copyUndirectedSparseGraph(G);
-		removePedantVertexes(G, I, Gp);
+		Graph<Node, Link> Gp = removePedantVertexes(G, I);
 		
-		/* Step 2: Choose a graph separator S ⊆ G'.V */
+		/* Step 2: Choose a graph separator S in G'.V */
 		
-		GraphSeparator graphSeparator = Separator.graphSeparator(Gp);
+		GraphSeparator graphSeparator = Separator.GraphPartitionAE(15, G ,50, 60, 100, 0.01, 0.1);
 		Set<Node> S = graphSeparator.getSeparator();
 		List<Graph<Node, Link>> G1m = graphSeparator.getComponents();
+		dijkstra = new DijkstraShortestPath<Node, Link>(Gp);
 		
 		/* Step 3: find a superset S+ of S */
-		
 		Set<Node> Splus = new HashSet<Node>(S);
-		for (Graph<Node, Link> Gi : G1m) { // Foreach connected componet
-			for (Node u : Gi.getVertices()) {
-				DijkstraShortestPath<Node, Link> dijkstra = new DijkstraShortestPath<Node, Link>(G); 
-				for (Node v : S) {
-					if (!Splus.contains(u)) {
-						List<Link> P = dijkstra.getPath(u, v);
-						for (Link link : P) {
-							try {
-								Node w = link.getDstNode();
-								if (w.getId() != u.getId()) {
-									Splus.add(w);
-								}
-							} catch (NodeNotFoundException e) {
-								e.printStackTrace();
-							}
+		
+		for (Graph<Node, Link> Gi : G1m)  // Foreach connected componet
+		{
+			for (Node u : Gi.getVertices()) 
+			{
+				for (Node v : S) 
+				{
+					if (!Splus.contains(u)) 
+					{
+						List<Node> P = toPathNode(u, v);
+						
+						for (Iterator<Node> ii = P.iterator() ; ii.hasNext() ; )
+						{
+							Node w = ii.next();
+							if (w != u && w != v) Splus.add(w);
 						}
 					}
 				}
@@ -60,47 +114,39 @@ public class BGPSepSAlgorithm extends BGPSepDAlgorithm {
 		}
 		
 		/* Step 4: Fully mesh the routers in S+ */
-		
 		for (Node u : Splus) {
 			for (Node v : Splus) {
-				if (u != v) {
+				if (u != v && !contiene (I, u.getId(), v.getId())) {
 					I.add(new iBGPSession(u.getId(), v.getId(), iBGPSessionType.peer));
 				}
 			}
 		}
 		
-		/* Step 5: Let every router in G'.V − S+ be a route
+		/* Step 5: Let every router in G'.V - S+ be a route
 		reflector client of some routers in S+ */
 		
 		Set<Node> GpV_Splus = new HashSet<Node>(Gp.getVertices());
 		GpV_Splus.removeAll(Splus);
-		for (Node u : GpV_Splus) {
-			DijkstraShortestPath<Node, Link> dijkstra = new DijkstraShortestPath<Node, Link>(G);
-			for (Node v : Splus) {
-				List<Link> P = dijkstra.getPath(u, v);
-				Node Ri = null;
-				for (Link link : P) {
-					try {
-						Ri = link.getDstNode();
-						if (Splus.contains(Ri)) {
-							break;
-						}
-					} catch (NodeNotFoundException e) {
-						e.printStackTrace();
-					}
-				}
+		for (Node u : GpV_Splus) 
+		{
+			for (Node v : Splus) 
+			{
+				List<Node> P = toPathNode(u, v);
+				Iterator<Node> ii = P.iterator();
+				Node Ri = ii.next(); // como el nodo u0, estoy en u1 (el siguiente)
+				for ( ; ii.hasNext() && Splus.contains(Ri = ii.next()) ; );
 				I.add(new iBGPSession(u.getId(), Ri.getId(), iBGPSessionType.client));
 			}
 		}
 
-		/* Step 6: full mesh routers in Gi − S+ */
+		/* Step 6: full mesh routers in Gi - S+ */
 		
 		for (Graph<Node, Link> Gi : G1m) { // Foreach connected componet
 			Set<Node> GiV_Splus = new HashSet<Node>(Gi.getVertices());
 			GiV_Splus.removeAll(Splus);
 			for (Node u : GiV_Splus) {
 				for (Node v : GiV_Splus) {
-					if (u != v) {
+					if (u != v && !contiene (I, u.getId(), v.getId())) {
 						I.add(new iBGPSession(u.getId(), v.getId(), iBGPSessionType.peer));
 					}
 				}
