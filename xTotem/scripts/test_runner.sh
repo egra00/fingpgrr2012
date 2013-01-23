0559e97cd4cc0a180d2ed628547ecb333d140183
emilianogfcm <emilianogfcm@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2013-01-23 22:06:41 +0000

    Agrego los scripts

---
+++ xTotem/scripts/test_runner.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+test_path=$1
+totem_script=$2
+all_algoritms=(bgpsep bgpsepB bgpsepD bgpsepS cbr fullmesh optimal zhang)
+for test_file in `find ${test_path} -name '*.xml'`
+do 
+	for algoritm in "${all_algoritms[@]}"
+	do
+		echo "${totem_script} -rrloc_${algoritm} ${test_file}"
+	done
+done
+
