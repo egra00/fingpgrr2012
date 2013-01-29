0fa6f323fc0aa01d26e1a0b3b9c45bc1a3290be1
emilianogfcm <emilianogfcm@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2013-01-29 23:06:24 +0000

    Correcciones a los script y subo los resultados de la primer ejecución completa. (QDNA)

--- xTotem/test_runner.sh
+++ xTotem/test_runner.sh
@@ -1,13 +1,32 @@
 #! /bin/bash
 
+if [ $# -lt 2 ]; then
+	echo "Usage $0 totem_runner topologies_path"
+	exit -1
+fi
+
 test_path=$1
 totem_script=$2
-all_algoritms=(bgpsep bgpsepB bgpsepD bgpsepS cbr fullmesh optimal zhang)
-for test_file in `find ${test_path} -name '*.xml'`
+debug=$3
+all_algoritms=(bgpsep bgpsepB bgpsepD bgpsepS fullmesh optimal zhang bates batesY batesZ) 
+
+mode="2>&1 /dev/null"
+if [ ${debug} -a ${debug} = "-vv" ]; then
+	mode="> /dev/null"
+fi
+if [ ${debug} -a ${debug} = "-vvv" ]; then
+        mode=""
+fi
+
+
+
+for test_file in `find ${test_path} -regextype awk -mtime -1 ! -regex  '.*(svn|BGPSep|BGPSepBackbone|BGPSepD|BGPSepS|Cbr|FullMesh|Optimal|Zhang|Bates|BatesY|BatesZ).*' -regex '.*xml$'`
 do 
 	for algoritm in "${all_algoritms[@]}"
 	do
-		`${totem_script} -rrloc_${algoritm} ${test_file}`
+		echo "[${totem_script}]: Starting ${algoritm} with ${test_file}"
+		eval "${totem_script} -rrloc_${algoritm} ${test_file} ${mode}"
+		echo "[${totem_script}]: End ${algoritm} with ${test_file}"
 	done
 done
 
