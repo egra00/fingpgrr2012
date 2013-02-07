fe1d9becc838cc2410f17361198564d6d09feec2
emilianogfcm@gmail.com <emilianogfcm@gmail.com@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2013-02-07 23:22:09 +0000

    Agrego una planilla donde se incuirán los test y comienzo a esribir el scrip para ejecutar los test. Todavía no funciona.

---
+++ xTotem/calibrator.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+if [ $# -lt 3 ]; then
+	echo "Usage $0 totem_runner topologies_path tests_path"
+	exit -1
+fi
+
+totem_script=$1
+topologies_path=$2
+test_file=$3
+debug=$4
+all_algoritms=(bgpsep bgpsepB bgpsepD bgpsepS) 
+
+mode="2>&1 /dev/null"
+if [ ${debug} -a ${debug} = "-vv" ]; then
+	mode="> /dev/null"
+fi
+if [ ${debug} -a ${debug} = "-vvv" ]; then
+        mode=""
+fi
+
+for test_file in `find ${topologies_path} -regextype awk -mtime -1 ! -regex  '.*(svn|BGPSep|BGPSepBackbone|BGPSepD|BGPSepS|Cbr|FullMesh|Optimal|Zhang|Bates|BatesY|BatesZ).*' -regex '.*xml$'`
+do 
+	for algoritm in "${all_algoritms[@]}"
+	do
+		while read params           
+		do
+			#eval "${totem_script} -rrloc_${algoritm} ${test_file} ${params} ${mode}"
+			echo "${totem_script} -rrloc_${algoritm} ${test_file} ${params} ${mode}"
+		done < ${test_file}
+	done
+done
+
