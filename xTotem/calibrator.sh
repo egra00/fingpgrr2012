805d171ecbae7e2a1ff3c8a5c16cd053ec239f7f
emilianogfcm <emilianogfcm@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2013-02-12 22:55:31 +0000

    Muevo algunas cosas para que queden mas comodas

--- xTotem/calibrator.sh
+++ xTotem/calibrator.sh
@@ -33,19 +33,23 @@ if [ ${debug} -a ${debug} = "-vvv" ]; then
 fi
 
 for one_topology_file in `find ${topologies_and_tras_path} -regextype awk -mtime -1 ! -regex  '.*(svn|BGPSep|BGPSepBackbone|BGPSepD|BGPSepS|Cbr|FullMesh|Optimal|Zhang|Bates|BatesY|BatesZ).*' -regex '.*xml$'`
-do 
-	one_tra_file="${one_topology_file::3}.tra"
-	if [ -e ${one_tra_file} ]; then
+do
+	echo ${one_topology_file}
+	one_tra_file="${one_topology_file%.*}.tra"
+	echo ${one_tra_file}
+	if [ ! -f ${one_tra_file} ]; then
 		echo "ERROR: Not existe the file "
 		exit -1
 	fi
 	for algoritm in "${all_algoritms[@]}"
 	do
-		one_output_name=`basename ${one_topology_file::3}${algoritm}`
-		while read params           
+		base_output_name=`basename ${one_topology_file%.*}_${algoritm}`
+		unique_file=0
+		while read params
 		do
-			#eval "${totem_script} -rrloc_${algoritm} ${one_topology_file} -tra ${one_tra_file} -ofn ${params} ${mode}"
-			echo "${totem_script} -rrloc_${algoritm} ${one_topology_file} -tra ${one_tra_file} -ofn ${params} ${mode}"
+			#eval "${totem_script} -rrloc_${algoritm} ${one_topology_file} -tra ${one_tra_file} -ofn ${base_output_name}_${unique_file} ${params} ${mode}"
+			echo "${totem_script} -rrloc_${algoritm} ${one_topology_file} -tra ${one_tra_file} -ofn ${base_output_name}_${unique_file} ${params} ${mode}"
+			((unique_file++))
 		done < ${test_file}
 	done
 done
