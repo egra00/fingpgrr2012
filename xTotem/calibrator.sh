19c17442c48f5a37abaa445e90f78f62c6673d22
emilianogfcm@gmail.com <emilianogfcm@gmail.com@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2013-02-14 23:56:57 +0000

    Agrego la instancia de prueba para grasp. Además hago cambios en el calibrato.sh para permitir utilizar mas de un archivo de parámetros (uno para grasp - grasp.params, el que agrego ahora - una para el ae y otro para el kmedoils)

--- xTotem/calibrator.sh
+++ xTotem/calibrator.sh
@@ -1,21 +1,26 @@
 #! /bin/bash
 
-################################################################################
-# En la carpeta topologies_and_tras_path estarán las topologías sobre las que  #
-# se va a ejecutár la calibración y para cada topología example.xml debe       #
-# existir un archivo example.tra con la definición del escenario para esta     #
-# topología                                                                    #
-################################################################################
+####################################################################################
+# En la carpeta topologies_and_tras_path estarán las topologías sobre las que      #
+# se va a ejecutár la calibración y para cada topología example.xml debe           #
+# existir un archivo example.tra con la definición del escenario para esta         #
+# topología                                                                        #
+####################################################################################
+#                                                                                  #
+# Para ejecutar la calibración de los alogritmos con las instancias presentadas:   #
+# ./calibrator.sh ./totem.sh ./parser.pl calibration/topologies calibration/params #
+#                                                                                  #
+####################################################################################
 
 if [ $# -lt 4 ]; then
-	echo "Usage $0 totem_runner parser topologies_and_tras_path tests_path"
+	echo "Usage $0 totem_runner parser topologies_and_tras_path params_path"
 	exit -1
 fi
 
 totem_script=$1
 parser_script=$2
 topologies_and_tras_path=$3
-test_file=$4
+params_path=$4
 debug=$5
 #all_algoritms=(bgpsep bgpsepB bgpsepD bgpsepS) 
 all_algoritms=(bgpsep)
@@ -41,18 +46,21 @@ do
 		topology_name=`basename ${one_topology_file%.*}`
 		base_output_dir=`dirname ${one_topology_file}`
 		unique_file=0
-		while read params
-		do
-			eval "${totem_script} -rrloc_${algoritm} ${one_topology_file} -tra ${one_tra_file} -ofn ${base_output_name}_${unique_file} ${params} ${mode}"
-			#echo "${totem_script} -rrloc_${algoritm} ${one_topology_file} -tra ${one_tra_file} -ofn ${base_output_name}_${unique_file} ${params} ${mode}"
-			eval "cbgp -c ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}.cli > ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}.bgp"
-			#echo "cbgp -c ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}.cli > ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}.bgp"
+		for one_params_file in `find ${params_path} -regextype awk -not -regex '*.params$'`
+			base_params_file=`basename ${one_params_file%.*}`
+			while read params
+			do
+				eval "${totem_script} -rrloc_${algoritm} ${one_topology_file} -tra ${one_tra_file} -ofn ${base_output_name}_${base_params_file}_${unique_file} ${params} ${mode}"
+				#echo "${totem_script} -rrloc_${algoritm} ${one_topology_file} -tra ${one_tra_file} -ofn ${base_output_name}_${base_params_file}_${unique_file} ${params} ${mode}"
+				eval "cbgp -c ${base_output_dir}/${topology_name}-${base_output_name}_${base_params_file}_${unique_file}.cli > ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}.bgp"
+				#echo "cbgp -c ${base_output_dir}/${topology_name}-${base_output_name}_${base_params_file}_${unique_file}.cli > ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}.bgp"
 
-			eval "${parser_script} ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}.bgp ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}"
-			#echo "${parser_script} ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}.bgp ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}"
+				eval "${parser_script} ${base_output_dir}/${topology_name}-${base_output_name}_${base_params_file}_${unique_file}.bgp ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}"
+				#echo "${parser_script} ${base_output_dir}/${topology_name}-${base_output_name}_${base_params_file}_${unique_file}.bgp ${base_output_dir}/${topology_name}-${base_output_name}_${unique_file}"
 
-			((unique_file++))
-		done < ${test_file}
+				((unique_file++))
+			done < ${one_params_file}
+		done
 	done
 done
 
