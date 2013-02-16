1d27a2f660b40868f683edca8a6f17669480a655
emilianogfcm@gmail.com <emilianogfcm@gmail.com@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2013-02-16 21:40:02 +0000

    Imprimo, además del pid el nombre de la máquina donde corre

--- xTotem/calibrator_runner.sh
+++ xTotem/calibrator_runner.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+host=hostname
 (	trap "" HUP
 	exec 0< /dev/null
 	exec 1> calibrator.log
 	exec 2> calibrator.log
 	./calibrator.sh ./totem.sh ./parser.pl calibration/topologies calibration/params
 ) &
-echo "##### Ejecutando con pid $! #####" > calibrator.pid
+echo "##### Ejecutando en ${host} con pid $! #####" > calibrator.pid
 
