313dee5897e06b380538b5479ee96c6cabb827bc
emilianogfcm@gmail.com <emilianogfcm@gmail.com@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2013-03-02 19:09:47 +0000

    Elimino todos los warnings del paquete nuestro

--- xTotem/src/uy/edu/fing/repository/rrloc/algorithms/batesX/batesZ/BatesZ.java
+++ xTotem/src/uy/edu/fing/repository/rrloc/algorithms/batesX/batesZ/BatesZ.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
