d827a6057950ed23daa824711b07e34a2b436942
emilianogfcm@gmail.com <emilianogfcm@gmail.com@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2013-03-26 01:37:47 +0000

    Agrego las topologías de purbas, los tra vacíos (solo con los routers de borde), la tabla que describe las topologías y el script para correr

---
+++ xTotem/scripts/prove_runner.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+host=`hostname`
+(	trap "" HUP
+	exec 0< /dev/null
+	exec 1> calibrator.log
+	exec 2> calibrator.log
+	./runner.sh ./totem.sh ./parser.pl ../prove/topologies -f -bgpsepD -bgpsepS -fullmesh -optimal $@
+) &
+echo "##### Ejecutando en ${host} con pid $! #####" > $0.pid
+
