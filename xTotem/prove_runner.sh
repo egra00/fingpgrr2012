2faf096c0942c70b5a4ce72566325c56ce092792
emilianogfcm <emilianogfcm@7b2fd428-036f-6b23-e5b4-885ae1bdede4>
2013-03-28 00:22:50 +0000

    Corrijo el script de pruebas

--- xTotem/prove_runner.sh
+++ xTotem/prove_runner.sh
@@ -4,7 +4,7 @@ host=`hostname`
 	exec 0< /dev/null
 	exec 1> calibrator.log
 	exec 2> calibrator.log
-	./runner.sh ./totem.sh ./parser.pl ../prove/topologies -f -bgpsepD -bgpsepS -fullmesh -optimal $@
+	./runner.sh ./totem.sh ./parser.pl prove/topologies -f -bgpsepD -bgpsepS -fullmesh -optimal $@
 ) &
 echo "##### Ejecutando en ${host} con pid $! #####" > $0.pid
 
